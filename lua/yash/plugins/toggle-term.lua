71158aabdf97261c7bcbe41c13b84c868657142e
yash91989201 <yashraj6370@gmail.com>
2024-08-06 07:17:16 +0000

    update sql lsp, change toggleterm float size

--- lua/yash/plugins/toggle-term.lua
+++ lua/yash/plugins/toggle-term.lua
@@ -16,28 +16,36 @@ return {
 			auto_scroll = true,
 			float_opts = {
 				border = "single",
-				width = 96,
-				height = 24,
+				width = 108,
+				height = 32,
 				winblend = 3,
-				title_pos = "center"
+				title_pos = "center",
 			},
 			winbar = {
 				enabled = false,
 				name_formatter = function(term) --  term: Terminal
 					return term.name
-				end
+				end,
 			},
 		})
 
-
 		-- set keymaps
-		keymap.set("n", "<leader>xv", "<cmd>ToggleTerm direction=vertical size=64<CR>", { desc = "Toggle vertical terminal" })
-		keymap.set("n", "<leader>xh", "<cmd>ToggleTerm direction=horizontal size=24<CR>",
-			{ desc = "Toggle horizontal terminal" })
+		keymap.set(
+			"n",
+			"<leader>xv",
+			"<cmd>ToggleTerm direction=vertical size=64<CR>",
+			{ desc = "Toggle vertical terminal" }
+		)
+		keymap.set(
+			"n",
+			"<leader>xh",
+			"<cmd>ToggleTerm direction=horizontal size=24<CR>",
+			{ desc = "Toggle horizontal terminal" }
+		)
 		keymap.set("n", "<leader>xf", "<cmd>ToggleTerm direction=float<CR>", { desc = "Toggle floating terminal" })
 		keymap.set("n", "<leader>xn", "<cmd>ToggleTermSetName<CR>", { desc = "Set terminal name" })
 		keymap.set("n", "<leader>xs", "<cmd>TermSelect<CR>", { desc = "Select between terminal" })
 		keymap.set("n", "<leader>xx", "<cmd>ToggleTerm<CR>", { desc = "Toggle between terminal" })
-		keymap.set('t', '<Esc>', "<C-\\><C-n><C-w>h", { silent = true, desc = "Normal mode in terminal" })
-	end
+		keymap.set("t", "<Esc>", "<C-\\><C-n><C-w>h", { silent = true, desc = "Normal mode in terminal" })
+	end,
 }
