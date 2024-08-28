7546b0d883b070a1086673d8600fa52c7c51931d
yash91989201 <yashraj6370@gmail.com>
2024-08-28 08:28:02 +0530

    changed plugins lua file names to names of plugins

--- lua/yash/lazy.lua
+++ lua/yash/lazy.lua
@@ -1,26 +1,25 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- get the latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- get the latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { import = "yash.plugins"},
-  { import = "yash.plugins.lsp"},
-  checker = {
-    enabled = true, 
-    notify = false,
-  },
-  change_detection = {
-    notify = false, 
-  },
+	{ import = "yash.plugins" },
+	{ import = "yash.plugins.lsp" },
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
-
