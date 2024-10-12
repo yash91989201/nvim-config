2ef33dd465f277cb3b5090f54d9734e019c06973
yash91989201 <yashraj6370@gmail.com>
2024-10-12 09:06:28 +0530

    move yamlls config to ftplugin/yaml from lsp-config

--- lua/yash/plugins/ftconfig/yaml.lua
+++ lua/yash/plugins/ftconfig/yaml.lua
@@ -46,7 +46,12 @@ require("lspconfig").yamlls.setup({
 			schemas = {
 				kubernetes = "k8s-*.yaml",
 				["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-				-- Add more schemas here...
+				["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+				["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
+				["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+				["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+				["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+				["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
 			},
 		},
 	},
