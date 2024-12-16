bd355d47ea0776de88378531bdbe8c7d020708d4
yash91989201 <yashraj6370@gmail.com>
2024-12-16 18:25:34 +0530

    change kulala key to h

--- lua/yash/plugins/kulala.lua
+++ lua/yash/plugins/kulala.lua
@@ -2,11 +2,11 @@ return {
 	"mistweaverco/kulala.nvim",
 	ft = "http",
 	keys = {
-		{ "<leader>r", "", desc = "+Rest" },
-		{ "<leader>rs", "<cmd>lua require('kulala').run()<cr>", desc = "Send the request" },
-		{ "<leader>rt", "<cmd>lua require('kulala').toggle_view()<cr>", desc = "Toggle headers/body" },
-		{ "<leader>rp", "<cmd>lua require('kulala').jump_prev()<cr>", desc = "Jump to previous request" },
-		{ "<leader>rn", "<cmd>lua require('kulala').jump_next()<cr>", desc = "Jump to next request" },
+		{ "<leader>h", "", desc = "+Rest" },
+		{ "<leader>hs", "<cmd>lua require('kulala').run()<cr>", desc = "Send the request" },
+		{ "<leader>ht", "<cmd>lua require('kulala').toggle_view()<cr>", desc = "Toggle headers/body" },
+		{ "<leader>hp", "<cmd>lua require('kulala').jump_prev()<cr>", desc = "Jump to previous request" },
+		{ "<leader>hn", "<cmd>lua require('kulala').jump_next()<cr>", desc = "Jump to next request" },
 	},
 	opts = {},
 }
