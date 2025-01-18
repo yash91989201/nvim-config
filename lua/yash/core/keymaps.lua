381bc82ec5f8f9af0d0440abac3feda6b451624f
yash91989201 <yashraj6370@gmail.com>
2025-01-18 11:07:41 +0530

    feat: add tab handling keybindings
    tt for closing all tabs execpt the current one
    tl for closing all tabs left to the current tab
    tl for closing all tabs right of the current tab

--- lua/yash/core/keymaps.lua
+++ lua/yash/core/keymaps.lua
@@ -2,23 +2,50 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("i","jk","<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights"})
+keymap.set("n", "<leader>ch", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", {desc = "Increment number"})
-keymap.set("n", "<leader>-", "<C-x>", {desc = "Decrement number"})
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically"})
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally"})
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size"})
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split"})
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab"})
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab"})
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab"})
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab"})
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab"})
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 
+-- Close all tabs to the right of the current tab
+keymap.set("n", "<leader>tl", function()
+	local current = vim.fn.tabpagenr()
+	local total = vim.fn.tabpagenr("$")
+	for i = total, current + 1, -1 do
+		vim.cmd("tabclose " .. i)
+	end
+end, { desc = "Close all tabs to the right" })
+
+-- Close all tabs to the left of the current tab
+keymap.set("n", "<leader>th", function()
+	local current = vim.fn.tabpagenr()
+	for i = current - 1, 1, -1 do
+		vim.cmd("tabclose " .. i)
+	end
+end, { desc = "Close all tabs to the left" })
+
+-- Close all tabs other than the current tab
+keymap.set("n", "<leader>tt", function()
+	local current = vim.fn.tabpagenr()
+	local total = vim.fn.tabpagenr("$")
+	for i = total, 1, -1 do
+		if i ~= current then
+			vim.cmd("tabclose " .. i)
+		end
+	end
+end, { desc = "Close all other tabs" })
