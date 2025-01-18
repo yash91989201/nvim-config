088759966b9c17c158b112db4960495465ef37d2
yash91989201 <yashraj6370@gmail.com>
2025-01-18 11:03:10 +0530

    fix: set filetype pattern so that .env .env.example etc type files get recognized as dotenv files

--- lua/yash/core/filetype.lua
+++ lua/yash/core/filetype.lua
@@ -5,4 +5,7 @@ filetype.add({
 		["http"] = "http",
 		["tf"] = "terraform",
 	},
+	pattern = {
+		[".*%.env.*"] = "dotenv", -- Matches files like `.env`, `.env.local`, `.env.example`, etc.
+	},
 })
