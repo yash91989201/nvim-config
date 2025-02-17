ae0187430e9401a97d07fbc3d7aa03773c6899b2
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-02-17 07:00:42 +0000

    remove svelte from config

--- lua/yash/plugins/lsp/mason.lua
+++ lua/yash/plugins/lsp/mason.lua
@@ -1,63 +1,61 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		-- import mason
-		local mason = require("mason")
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    -- import mason
+    local mason = require("mason")
 
-		-- import mason-lspconfig
-		local mason_lspconfig = require("mason-lspconfig")
+    -- import mason-lspconfig
+    local mason_lspconfig = require("mason-lspconfig")
 
-		local mason_tool_installer = require("mason-tool-installer")
+    local mason_tool_installer = require("mason-tool-installer")
 
-		-- enable mason and configure icons
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+    -- enable mason and configure icons
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
 
-		mason_lspconfig.setup({
-			automatic_installation = false,
-			-- list of servers for mason to install
-			ensure_installed = {
-				"html",
-				"cssls",
-				"tailwindcss",
-				"svelte",
-				"lua_ls",
-				"graphql",
-				"emmet_ls",
-				"prismals",
-				"ansiblels",
-				"bashls",
-				"dockerls",
-				"docker_compose_language_service",
-				"eslint",
-				"golangci_lint_ls",
-				"gopls",
-				"helm_ls",
-				"jsonls",
-				"sqlls",
-				"taplo",
-				"tflint",
-				"yamlls",
-			},
-		})
+    mason_lspconfig.setup({
+      automatic_installation = false,
+      -- list of servers for mason to install
+      ensure_installed = {
+        "html",
+        "cssls",
+        "tailwindcss",
+        "lua_ls",
+        "graphql",
+        "emmet_ls",
+        "ansiblels",
+        "bashls",
+        "dockerls",
+        "docker_compose_language_service",
+        "eslint",
+        "golangci_lint_ls",
+        "gopls",
+        "helm_ls",
+        "jsonls",
+        "sqlls",
+        "taplo",
+        "tflint",
+        "yamlls",
+      },
+    })
 
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"prettier", -- prettier formatter
-				"stylua", -- lua formatter
-				"eslint_d",
-			},
-		})
-	end,
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "eslint_d",
+      },
+    })
+  end,
 }
